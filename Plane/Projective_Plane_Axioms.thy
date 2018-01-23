d4f8ff7d4d61dfbd5362b57f686f268a7a955fff
Anthony Bordg <bordg.anthony@gmail.com>
2018-01-23 18:50:22 +0000

    Update Projective_Plane_Axioms

--- Plane/Projective_Plane_Axioms.thy
+++ Plane/Projective_Plane_Axioms.thy
@@ -2,16 +2,22 @@ theory Projective_Plane_Axioms
   imports Main
 begin
 
-(** The axioms of the Projective Plane **)
+(* Contents:
+- We introduce the types of points and lines and an incidence relation between them.
+- A set of axioms for the projective plane (the models of these axioms are 
+n-dimensional with n \<ge> 2). 
+*)
+
+(** The Axioms of the Projective Plane **)
 
 (* One has a type of points *)
-typedecl "points"
+typedecl "Points"
 
 (* One has a type of lines *)
-typedecl "lines"
+typedecl "Lines"
 
 (* One has an incidence relation between points and lines *)
-consts incid :: "points \<Rightarrow> lines \<Rightarrow> bool"
+consts incid :: "Points \<Rightarrow> Lines \<Rightarrow> bool"
 
 (* The incidence relation is decidable *)
 axiomatization where
@@ -19,18 +25,18 @@ incid_dec: "\<forall>P l. incid P l \<or> \<not>(incid P l)"
 
 (* Ax1: Any two (distinct) points lie on a (unique) line *)
 axiomatization where
-ax1: "\<forall>P Q.\<exists>l. incid P l \<and> incid Q l"
+ax1: "\<forall>P Q. \<exists>l. incid P l \<and> incid Q l"
 
 (* Ax2: Any two (distinct) lines meet in a (unique) point *)
 axiomatization where
-ax2: "\<forall>l m.\<exists>P. incid P l \<and> incid P m"
+ax2: "\<forall>l m. \<exists>P. incid P l \<and> incid P m"
 
 (* The uniqueness part *)
 axiomatization where
 ax_uniqueness: "\<forall>P Q l m. incid P l \<longrightarrow> incid Q l \<longrightarrow> incid P m \<longrightarrow> incid Q m \<longrightarrow>
 P = Q \<or> l = m"
 
-definition distinct4 :: "points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> bool" where
+definition distinct4 :: "Points \<Rightarrow> Points \<Rightarrow> Points \<Rightarrow> Points \<Rightarrow> bool" where
 "distinct4 A B C D \<equiv> (A \<noteq> B) \<and> (A \<noteq> C) \<and> (A \<noteq> D) \<and> (B \<noteq> C) \<and> (B \<noteq> D) \<and> (C \<noteq> D)"
 
 (* Ax3: There exists four points such that no three of them are collinear *)
