40cb0e6064e707f8b3cf59db6a15fe5c140362d6
Anthony Bordg <bordg.anthony@gmail.com>
2018-03-20 17:35:31 +0000

    Update Higher_Proj_Space_Rank_Axioms_To_Higher_Proj_Space_Axioms.thy

--- Space/Higher_Proj_Space_Rank_Axioms_To_Higher_Proj_Space_Axioms.thy
+++ Space/Higher_Proj_Space_Rank_Axioms_To_Higher_Proj_Space_Axioms.thy
@@ -120,10 +120,10 @@ qed
 
 lemma ax1_uniqueness : "\<forall>P Q k l. 
 (incid P k) \<longrightarrow> (incid Q k) \<longrightarrow> (incid P l) \<longrightarrow> (incid Q l) \<longrightarrow> (P = Q) \<or> (line_eq k l)"
-proof-
+proof(rule allI, rule allI, rule allI, rule allI, rule impI, rule impI, rule impI, rule impI)
   fix P Q k l
   assume "incid P k" and "incid Q k" and "incid P l" and "incid Q l"
-  have "line_eq k l" if "P \<noteq> Q"
+  have f1:"line_eq k l" if "P \<noteq> Q"
   proof-
     have f1:"rk {P, Q} = 2"
       by (simp add: rk_couple that)
@@ -137,7 +137,69 @@ proof-
     from f1 and f2 and f3 and f4 have "rk {fst k, snd k, fst l, snd l, P, Q} \<le> 2" by auto
     thus "line_eq k l" using matroid_ax_2 by (smt eq_iff f3 insert_mono insert_subset line_eq_def)
   qed
-  show "P = Q \<or> line_eq k l"
+  from f1 show "P = Q \<or> line_eq k l" by auto
+qed
+
+definition distinct4 :: "points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> bool" where
+"distinct4 A B C D \<equiv> (A \<noteq> B) \<and> (A \<noteq> C) \<and> (A \<noteq> D) \<and> (B \<noteq> C) \<and> (B \<noteq> D) \<and> (C \<noteq> D)"
+
+lemma rk_lines_inter :
+  assumes "incid P k" and "incid P l"
+  shows "rk {fst k, snd k, fst l, snd l} \<le> 3"
+proof-
+  have f1:"rk {fst k, snd k, P} \<le> 2"
+    using assms(1) incid_def by auto
+  have f2:"rk {fst l, snd l, P} \<le> 2"
+    using assms(2) incid_def by auto
+  have f3:"rk {fst k, snd k, fst l, snd l, P} + rk {P} \<le> rk {fst k, snd k, P} + rk {fst l, snd l, P}" 
+    using matroid_ax_3_alt[of "{fst k, snd k, P}" "{fst l, snd l, P}" "{P}"] sorry
+    by (smt Int_commute Int_insert_right Un_commute Un_insert_left inf_absorb2 insert_subset matroid_ax_3_alt subset_insertI sup_inf_absorb)
+  then have "rk {fst k, snd k, fst l, snd l, P} \<le> 3" 
+    using f1 f2 by (simp add: rk_singleton)
+  thus "rk {fst k, snd k, fst l, snd l} \<le> 3" using matroid_ax_2
+    by (metis insert_commute order_trans subset_insertI)
+qed
+
+lemma ax_2 : "\<forall>A B C D lAB lCD lAC lBD. distinct4 A B C D \<longrightarrow> (incid A lAB \<and> incid B lAB) 
+\<longrightarrow> (incid C lCD \<and> incid D lCD) \<longrightarrow> (incid A lAC \<and> incid C lAC) \<longrightarrow> 
+(incid B lBD \<and> incid D lBD) \<longrightarrow> (\<exists>I.(incid I lAB \<and> incid I lCD)) \<longrightarrow> 
+(\<exists>J.(incid J lAC \<and> incid J lBD))"
+proof(rule allI, rule allI, rule allI, rule allI, rule allI, rule allI, rule allI, rule allI, 
+rule impI, rule impI, rule impI, rule impI, rule impI, rule impI)
+  fix A B C D lAB lCD lAC lBD
+  assume "distinct4 A B C D" and "incid A lAB \<and> incid B lAB" and "incid C lCD \<and> incid D lCD" and
+"incid A lAC \<and> incid C lAC" and "incid B lBD \<and> incid D lBD" and "\<exists>I. incid I lAB \<and> incid I lCD"
+  show "\<exists>J. incid J lAC \<and> incid J lBD"
+  proof
+    obtain I where "incid I lAB \<and> incid I lCD"
+      using \<open>\<exists>I. incid I lAB \<and> incid I lCD\<close> by auto
+    have f1:"rk {fst lAB, snd lAB, A} = 2"
+      by (simp add: \<open>incid A lAB \<and> incid B lAB\<close> incid_rk)
+    have f2:"rk {fst lAB, snd lAB, B} = 2"
+      by (simp add: \<open>incid A lAB \<and> incid B lAB\<close> incid_rk)
+    from f1 and f2 have c1:"rk {fst lAB, snd lAB, A, B} = 2" using matroid_ax_3_alt'
+      by (metis Un_insert_right insert_absorb2 insert_commute line_eq_refl line_eq_rk sup_bot.right_neutral)
+    have f3:"rk {fst lCD, snd lCD, C} = 2"
+      by (simp add: \<open>incid C lCD \<and> incid D lCD\<close> incid_rk)
+    have f4:"rk {fst lCD, snd lCD, D} = 2"
+      by (simp add: \<open>incid C lCD \<and> incid D lCD\<close> incid_rk)
+    from f3 and f4 have c2:"rk {fst lCD, snd lCD, C, D} = 2" using matroid_ax_3_alt'
+      by (metis (no_types, hide_lams) Un_insert_right insert_absorb2 insert_commute line_eq_refl line_eq_rk sup_bot.right_neutral)
+    have f5:"rk {fst lAC, snd lAC, A} = 2"
+      by (simp add: \<open>incid A lAC \<and> incid C lAC\<close> incid_rk)
+    have f6:"rk {fst lAC, snd lAC, C} = 2"
+      by (simp add: \<open>incid A lAC \<and> incid C lAC\<close> incid_rk)
+    from f5 and f6 have c3:"rk {fst lAC, snd lAC, A, C} = 2" using matroid_ax_3_alt'
+      by (metis Un_insert_right insert_absorb2 insert_commute line_eq_refl line_eq_rk sup_bot.right_neutral)
+    have f7:"rk {fst lBD, snd lBD, B} = 2"
+      by (simp add: \<open>incid B lBD \<and> incid D lBD\<close> incid_rk)
+    have f8:"rk {fst lBD, snd lBD, D} = 2"
+      by (simp add: \<open>incid B lBD \<and> incid D lBD\<close> incid_rk)
+    from f7 and f8 have c4:"rk {fst lBD, snd lBD, B, D} = 2" using matroid_ax_3_alt'
+      by (metis Un_insert_right insert_absorb2 insert_commute line_eq_refl line_eq_rk sup_bot.right_neutral)
+    have "rk {fst lAB, snd lAB, fst lAC, snd lAC} \<le> 3"
+      using \<open>incid A lAB \<and> incid B lAB\<close> \<open>incid A lAC \<and> incid C lAC\<close> rk_lines_inter by blast
+    then have "rk {fst lAB, snd lAB, fst lAC, snd lAC, A} \<le> 3"
 
 
 
