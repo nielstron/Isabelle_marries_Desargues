e5a24080d6817f65decb256d08d0ffd8812d4e6e
Anthony Bordg <bordg.anthony@gmail.com>
2018-03-21 17:46:19 +0000

    Update Higher_Projective_Space_Rank_Axioms.thy

--- Space/Higher_Projective_Space_Rank_Axioms.thy
+++ Space/Higher_Projective_Space_Rank_Axioms.thy
@@ -10,7 +10,7 @@ consts rk :: "Points set \<Rightarrow> nat"
 
 (* The function rk satisfies the following axioms *)
 axiomatization where
-matroid_ax_1a: "\<forall>X. rk X \<ge> 0" and
+matroid_ax_1a: "\<forall>X. rk X \<ge> 0" (* Useless if rk is defined with values in \<nat>, not \<int> *) and
 matroid_ax_1b: "\<forall>X. rk X \<le> card X" and
 matroid_ax_2: "\<forall>X Y. X \<subseteq> Y \<longrightarrow> rk X \<le> rk Y" and
 matroid_ax_3: "\<forall>X Y. rk (X \<union> Y) + rk (X \<inter> Y) \<le> rk X + rk Y"
@@ -20,8 +20,12 @@ axiomatization where
 rk_ax_singleton: "\<forall>P. rk {P} \<ge> 1" and
 rk_ax_couple: "\<forall>P Q. P \<noteq> Q \<longrightarrow> rk {P,Q} \<ge> 2" and
 rk_ax_pasch: "\<forall>A B C D. rk {A,B,C,D} \<le> 3 \<longrightarrow> (\<exists>J. rk {A,B,J} = 2 \<and> rk {C,D,J} = 2)" and
-rk_axiom_3_points: "\<forall> A B. \<exists>C. rk {A,B,C} = 2 \<and> rk {B,C} = 2 \<and> rk {A,C} = 2" and
+rk_ax_3_pts: "\<forall> A B. \<exists>C. rk {A,B,C} = 2 \<and> rk {B,C} = 2 \<and> rk {A,C} = 2" and
 rk_ax_dim: "\<exists>A B C D. rk {A,B,C,D} \<ge> 4"
+
+(* Note that the rank-based axioms system above deals only with points. 
+Projective geometry developped this way is dimension-independent and it can be scaled to any dimension
+without adding any entity to the theory or modifying the language of the theory *)
 
 (*
 References:
