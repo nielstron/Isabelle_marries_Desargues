ea4a0d5f7770c6da8072d0c0190e8916980ec32e
Anthony Bordg <bordg.anthony@gmail.com>
2018-03-26 11:51:44 +0100

    Update Desargues_2D.thy

--- Space/Desargues_2D.thy
+++ Space/Desargues_2D.thy
@@ -80,6 +80,38 @@ proof-
     by blast
 qed
 
+lemma desargues_config_2D_new_pt :
+  assumes "desargues_config_2D A B C A' B' C' P \<alpha> \<beta> \<gamma>"
+  shows "\<exists>Q. rk {A, B, C, A', B', C', P, Q} \<ge> 4"
+proof-
+  have "rk {A, B, C, A', B', C', P} = 3" 
+    using assms coplanar_ABCA'B'C'P by auto
+  thus "\<exists>Q. rk {A, B, C, A', B', C', P, Q} \<ge> 4" 
+    using rk_ext
+    by (smt Un_insert_right add.commute add_One_commute insert_commute one_plus_numeral order_refl semiring_norm(2) semiring_norm(8) sup_bot.right_neutral)
+qed
+
+lemma rk_A'B'PQ :
+  assumes "desargues_config_2D A B C A' B' C' P \<alpha> \<beta> \<gamma>" and "rk {A', P} = 2" and "rk {B', P} = 2"
+and "rk {A, B, C, A', B', C', P, Q} \<ge> 4"
+  shows "rk {A', B', P, Q} = 4"
+proof-
+  have "card {A', B', P, Q} \<le> 4"
+    by (smt One_nat_def Suc_numeral card.insert card_empty finite.emptyI finite_insert insert_absorb insert_not_empty linear nat_add_left_cancel_le numeral_3_eq_3 numeral_Bit0 numeral_code(3) numeral_le_one_iff numerals(1) one_plus_numeral semiring_norm(4) semiring_norm(69) semiring_norm(70) semiring_norm(8))
+  then have f1:"rk {A', B', P, Q} \<le> 4" 
+    using matroid_ax_1b dual_order.trans by blast
+  have "rk {A, B, C, A', B', C', P, Q} + rk {A', B', P} \<le> rk {A', B', P, Q} + rk {A, B, C, A', B', C', P}"
+    using matroid_ax_3_alt[of "{A', B', P}" "{A', B', P, Q}" "{A, B, C, A', B', C', P}"]
+    by (simp add: insert_commute)
+  then have "rk {A', B', P, Q} \<ge> rk {A, B, C, A', B', C', P, Q} + rk {A', B', P} - rk {A, B, C, A', B', C', P}"
+    using le_diff_conv by blast
+  then have f2:"rk {A', B', P, Q} \<ge> 4" 
+    using assms(1) desargues_config_2D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] assms(2) assms(3) assms(4) non_colinear_A'B'P coplanar_ABCA'B'C'P
+    by (smt diff_add_inverse2 le_trans)
+  from f1 and f2 show "rk {A', B', P, Q} = 4"
+    by (simp add: f1 eq_iff)
+qed
+
 theorem desargues_2D :
   assumes "desargues_config_2D A B C A' B' C' P \<alpha> \<beta> \<gamma>"
   shows "rk {\<alpha>, \<beta>, \<gamma>} \<le> 2"
