4957fe7c91017488f85431670304e149aa5edfaf
Anthony Bordg <bordg.anthony@gmail.com>
2018-06-13 18:17:44 +0100

    Update Desargues_3D.thy

--- Space/Desargues_3D.thy
+++ Space/Desargues_3D.thy
@@ -23,9 +23,11 @@ lemma coplanar_4 :
   assumes "rk {A, B, C} = 3" and "rk {B, C, \<alpha>} = 2" 
   shows "rk {A, B, C, \<alpha>} = 3"
 proof-
-  have f1:"rk {A, B, C, \<alpha>} \<ge> 3" using matroid_ax_2
+  have f1:"rk {A, B, C, \<alpha>} \<ge> 3" 
+    using matroid_ax_2
     by (metis assms(1) empty_subsetI insert_mono)
-  have "rk {A, B, C, \<alpha>} + rk {B, C} \<le> rk {A, B, C} + rk {B, C, \<alpha>}" using matroid_ax_3_alt
+  have "rk {A, B, C, \<alpha>} + rk {B, C} \<le> rk {A, B, C} + rk {B, C, \<alpha>}" 
+    using matroid_ax_3_alt
     by (metis Un_insert_right add_One_commute add_mono assms(1) assms(2) matroid_ax_2_alt 
         nat_1_add_1 numeral_plus_one rk_singleton semiring_norm(3) sup_bot.right_neutral)
   then have f2:"rk {A, B, C, \<alpha>} \<le> 3"
@@ -85,10 +87,12 @@ lemma coplanar_5 :
   assumes "rk {A, B, C} = 3" and "rk {B, C, \<alpha>} = 2" and "rk {A, C, \<beta>} = 2"
   shows "rk {A, B, C, \<alpha>, \<beta>} = 3"
 proof-
-  have f1:"rk {A, B, C, \<alpha>} = 3" using coplanar_4
+  have f1:"rk {A, B, C, \<alpha>} = 3" 
+    using coplanar_4
     by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(2) insert_is_Un 
         le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
-  have f2:"rk {A, B, C, \<beta>} = 3" using coplanar_4_bis
+  have f2:"rk {A, B, C, \<beta>} = 3" 
+    using coplanar_4_bis
     by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(3) insert_is_Un 
         le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
   from f1 and f2 show "rk {A, B, C, \<alpha>, \<beta>} = 3" 
@@ -177,7 +181,8 @@ proof-
     by (simp add: insert_commute)
   then have "rk {\<alpha>, \<beta>, \<gamma>} \<le> rk {A, B, C, \<alpha>, \<beta>, \<gamma>} + rk {A', B', C', \<alpha>, \<beta>, \<gamma>} - rk {A, B, C, A', B', C', \<alpha>, \<beta>, \<gamma>}"
     by linarith
-  thus "rk {\<alpha>, \<beta>, \<gamma>} \<le> 2" using assms desargues_config_3D_coplanar_6 desargues_config_3D_non_coplanar
+  thus "rk {\<alpha>, \<beta>, \<gamma>} \<le> 2" 
+    using assms desargues_config_3D_coplanar_6 desargues_config_3D_non_coplanar
     by fastforce
 qed
 
