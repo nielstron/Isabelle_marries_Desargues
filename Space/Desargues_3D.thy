c0c0fac3d3e11687a74da7d9816316ff271118e9
Anthony Bordg <bordg.anthony@gmail.com>
2018-06-13 16:54:49 +0100

    Update Desargues_3D.thy

--- Space/Desargues_3D.thy
+++ Space/Desargues_3D.thy
@@ -12,7 +12,8 @@ the lines AA', BB' and CC' are concurrent in P), then they are perspective from 
 In this file we restrict ourself to the case where the two triangles ABC and A'B'C' are not coplanar. 
 *)
 
-definition desargues_config_3D :: "[Points, Points, Points, Points, Points, Points, Points, Points, Points, Points] => bool" 
+definition desargues_config_3D :: 
+  "[Points, Points, Points, Points, Points, Points, Points, Points, Points, Points] => bool" 
   where "desargues_config_3D A B C A' B' C' P \<alpha> \<beta> \<gamma> \<equiv> rk {A, B, C} = 3 \<and> rk {A', B', C'} = 3 \<and> 
 rk {A, A', P} = 2 \<and> rk {B, B', P} = 2 \<and> rk {C, C', P} = 2 \<and> rk {A, B, C, A', B', C'} \<ge> 4 \<and> 
 rk {B, C, \<alpha>} = 2 \<and> rk {B', C', \<alpha>} = 2 \<and> rk {A, C, \<beta>} = 2 \<and> rk {A', C', \<beta>} = 2 \<and> rk {A, B, \<gamma>} = 2 \<and>
@@ -25,10 +26,13 @@ proof-
   have f1:"rk {A, B, C, \<alpha>} \<ge> 3" using matroid_ax_2
     by (metis assms(1) empty_subsetI insert_mono)
   have "rk {A, B, C, \<alpha>} + rk {B, C} \<le> rk {A, B, C} + rk {B, C, \<alpha>}" using matroid_ax_3_alt
-    by (metis Un_insert_right add_One_commute add_mono assms(1) assms(2) matroid_ax_2_alt nat_1_add_1 numeral_plus_one rk_singleton semiring_norm(3) sup_bot.right_neutral)
+    by (metis Un_insert_right add_One_commute add_mono assms(1) assms(2) matroid_ax_2_alt 
+        nat_1_add_1 numeral_plus_one rk_singleton semiring_norm(3) sup_bot.right_neutral)
   then have f2:"rk {A, B, C, \<alpha>} \<le> 3"
-    by (metis Un_insert_right add_One_commute assms(2) matroid_ax_2_alt numeral_plus_one semiring_norm(3) sup_bot.right_neutral)
-  from f1 and f2 show "rk {A, B, C, \<alpha>} = 3" by auto
+    by (metis Un_insert_right add_One_commute assms(2) matroid_ax_2_alt numeral_plus_one 
+        semiring_norm(3) sup_bot.right_neutral)
+  from f1 and f2 show "rk {A, B, C, \<alpha>} = 3" 
+    by auto
 qed
 
 lemma desargues_config_3D_coplanar_4 :
@@ -36,9 +40,11 @@ lemma desargues_config_3D_coplanar_4 :
   shows "rk {A, B, C, \<alpha>} = 3" and "rk {A', B', C', \<alpha>} = 3"
 proof-
   show "rk {A, B, C, \<alpha>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4 by blast
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4 
+    by blast
   show "rk {A', B', C', \<alpha>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4 by blast
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4 
+    by blast
 qed
 
 lemma coplanar_4_bis :
@@ -51,9 +57,11 @@ lemma desargues_config_3D_coplanar_4_bis :
   shows "rk {A, B, C, \<beta>} = 3" and "rk {A', B', C', \<beta>} = 3"
 proof-
   show "rk {A, B, C, \<beta>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_bis by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_bis 
+    by auto
   show "rk {A', B', C', \<beta>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_bis by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_bis 
+    by auto
 qed
 
 lemma coplanar_4_ter :
@@ -66,9 +74,11 @@ lemma desargues_config_3D_coplanar_4_ter :
   shows "rk {A, B, C, \<gamma>} = 3" and "rk {A', B', C', \<gamma>} = 3"
 proof-
   show "rk {A, B, C, \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_ter by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_ter 
+    by auto
   show "rk {A', B', C', \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_ter by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_4_ter 
+    by auto
 qed
 
 lemma coplanar_5 :
@@ -76,9 +86,11 @@ lemma coplanar_5 :
   shows "rk {A, B, C, \<alpha>, \<beta>} = 3"
 proof-
   have f1:"rk {A, B, C, \<alpha>} = 3" using coplanar_4
-    by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(2) insert_is_Un le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
+    by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(2) insert_is_Un 
+        le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
   have f2:"rk {A, B, C, \<beta>} = 3" using coplanar_4_bis
-    by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(3) insert_is_Un le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
+    by (smt One_nat_def Un_assoc Un_commute add.commute add_Suc_right assms(1) assms(3) insert_is_Un 
+        le_antisym matroid_ax_2_alt numeral_2_eq_2 numeral_3_eq_3 one_add_one)
   from f1 and f2 show "rk {A, B, C, \<alpha>, \<beta>} = 3" 
     using matroid_ax_3_alt'
     by (metis Un_assoc assms(1) insert_is_Un)
@@ -89,9 +101,11 @@ lemma desargues_config_3D_coplanar_5 :
   shows "rk {A, B, C, \<alpha>, \<beta>} = 3" and "rk {A', B', C', \<alpha>, \<beta>} = 3"
 proof-
   show "rk {A, B, C, \<alpha>, \<beta>} = 3"
-    using assms desargues_config_3D_def coplanar_5 by auto
+    using assms desargues_config_3D_def coplanar_5 
+    by auto
   show "rk {A', B', C', \<alpha>, \<beta>} = 3"
-    using assms desargues_config_3D_def coplanar_5 by auto
+    using assms desargues_config_3D_def coplanar_5 
+    by auto
 qed
 
 
@@ -105,9 +119,11 @@ lemma desargues_config_3D_coplanar_5_bis :
   shows "rk {A, B, C, \<alpha>, \<gamma>} = 3" and "rk {A', B', C', \<alpha>, \<gamma>} = 3"
 proof-
   show "rk {A, B, C, \<alpha>, \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_5_bis by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_5_bis 
+    by auto
   show "rk {A', B', C', \<alpha>, \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_5_bis by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_5_bis 
+    by auto
 qed
 
 lemma coplanar_6 :
@@ -115,11 +131,14 @@ lemma coplanar_6 :
   shows "rk {A, B, C, \<alpha>, \<beta>, \<gamma>} = 3"
 proof-
   have f1:"rk {A, B, C, \<alpha>, \<gamma>} = 3" 
-    using coplanar_5_bis assms(1) assms(2) assms(3) by auto
+    using coplanar_5_bis assms(1) assms(2) assms(3) 
+    by auto
   have f2:"rk {A, B, C, \<alpha>, \<beta>} = 3"
-    using coplanar_5 assms(1) assms(2) assms(4) by auto
+    using coplanar_5 assms(1) assms(2) assms(4) 
+    by auto
   have f3:"rk {A, B, C, \<alpha>} = 3" 
-    using coplanar_4 assms(1) assms(2) by auto
+    using coplanar_4 assms(1) assms(2) 
+    by auto
   from f1 and f2 and f3 show "rk {A, B, C, \<alpha>, \<beta>, \<gamma>} = 3" 
     using matroid_ax_3_alt'[of "{A, B, C, \<alpha>}" "\<beta>" "\<gamma>"]
     by (metis Un_insert_left sup_bot.left_neutral)
@@ -130,9 +149,11 @@ lemma desargues_config_3D_coplanar_6 :
   shows "rk {A, B, C, \<alpha>, \<beta>, \<gamma>} = 3" and "rk {A', B', C', \<alpha>, \<beta>, \<gamma>} = 3"
 proof-
   show "rk {A, B, C, \<alpha>, \<beta>, \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_6 by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_6 
+    by auto
   show "rk {A', B', C', \<alpha>, \<beta>, \<gamma>} = 3"
-    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_6 by auto
+    using assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] coplanar_6 
+    by auto
 qed
 
 lemma desargues_config_3D_non_coplanar :
@@ -140,9 +161,11 @@ lemma desargues_config_3D_non_coplanar :
   shows "rk {A, B, C, A', B', C', \<alpha>, \<beta>, \<gamma>} \<ge> 4" 
 proof-
   have "rk {A, B, C, A', B', C'} \<le> rk {A, B, C, A', B', C', \<alpha>, \<beta>, \<gamma>}" 
-    using matroid_ax_2 by auto
+    using matroid_ax_2 
+    by auto
   thus "4 \<le> rk {A, B, C, A', B', C', \<alpha>, \<beta>, \<gamma>}" 
-    using matroid_ax_2 assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] by linarith
+    using matroid_ax_2 assms desargues_config_3D_def[of A B C A' B' C' P \<alpha> \<beta> \<gamma>] 
+    by linarith
 qed
 
 theorem desargues_3D :
