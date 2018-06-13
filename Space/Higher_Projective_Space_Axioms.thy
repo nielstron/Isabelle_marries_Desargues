22b4675bfeb231f4aa56ce426d5f286dd5a29fe3
Anthony Bordg <bordg.anthony@gmail.com>
2018-06-13 17:08:04 +0100

    Update Higher_Projective_Space_Axioms.thy

--- Space/Higher_Projective_Space_Axioms.thy
+++ Space/Higher_Projective_Space_Axioms.thy
@@ -25,10 +25,9 @@ consts incid :: "points \<Rightarrow> lines \<Rightarrow> bool"
 
 axiomatization where
 (* Ax1: Any two distinct points are incident with just one line *)
-ax1_existence: "\<forall>P M.\<exists>l. (incid P l) \<and> (incid M l)"
+ax1_existence: "\<exists>l. (incid P l) \<and> (incid M l)"
 axiomatization where
-ax1_uniqueness: "\<forall>P M k l. 
-(incid P k) \<longrightarrow> (incid M k) \<longrightarrow> (incid P l) \<longrightarrow> (incid M l) \<longrightarrow> (P = M) \<or> (k = l)"
+ax1_uniqueness: "(incid P k) \<longrightarrow> (incid M k) \<longrightarrow> (incid P l) \<longrightarrow> (incid M l) \<longrightarrow> (P = M) \<or> (k = l)"
 
 definition distinct4 :: "points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> points \<Rightarrow> bool" where
 "distinct4 A B C D \<equiv> (A \<noteq> B) \<and> (A \<noteq> C) \<and> (A \<noteq> D) \<and> (B \<noteq> C) \<and> (B \<noteq> D) \<and> (C \<noteq> D)"
@@ -37,7 +36,7 @@ definition distinct4 :: "points \<Rightarrow> points \<Rightarrow> points \<Righ
 Sometimes this is called Pasch's axiom, but according to Wikipedia it is misleading
 since Pasch's axiom refers to something else. *)
 axiomatization where
-ax2: "\<forall>A B C D lAB lCD lAC lBD. distinct4 A B C D \<longrightarrow> (incid A lAB \<and> incid B lAB) 
+ax2: "distinct4 A B C D \<longrightarrow> (incid A lAB \<and> incid B lAB) 
 \<longrightarrow> (incid C lCD \<and> incid D lCD) \<longrightarrow> (incid A lAC \<and> incid C lAC) \<longrightarrow> 
 (incid B lBD \<and> incid D lBD) \<longrightarrow> (\<exists>I.(incid I lAB \<and> incid I lCD)) \<longrightarrow> 
 (\<exists>J.(incid J lAC \<and> incid J lBD))"
@@ -51,7 +50,7 @@ As I understand it, this axiom makes sure that lines are not degenerated into po
 and since it asks for three distinct points, not only 2, it captures the idea that
 lines have unlimited extent, i.e. there is always a point between two distinct points. *)
 axiomatization where
-ax3: "\<forall>l.\<exists>A B C. distinct3 A B C \<and> (incid A l) \<and> (incid B l) \<and> (incid C l)"
+ax3: "\<exists>A B C. distinct3 A B C \<and> (incid A l) \<and> (incid B l) \<and> (incid C l)"
 
 (* Ax4: There exists two lines that do not meet, 
 hence the geometry is at least 3-dimensional *)
