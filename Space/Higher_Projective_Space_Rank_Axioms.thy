13961d13e16fe886cfd29f23bc22846e3eafdf93
Anthony Bordg <bordg.anthony@gmail.com>
2018-06-13 17:11:21 +0100

    Update Higher_Projective_Space_Rank_Axioms.thy

--- Space/Higher_Projective_Space_Rank_Axioms.thy
+++ Space/Higher_Projective_Space_Rank_Axioms.thy
@@ -18,17 +18,17 @@ consts rk :: "Points set \<Rightarrow> nat"
 
 (* The function rk satisfies the following axioms *)
 axiomatization where
-matroid_ax_1a: "\<forall>X. rk X \<ge> 0" (* Useless if rk is defined with values in \<nat>, not \<int> *) and
-matroid_ax_1b: "\<forall>X. rk X \<le> card X" and
-matroid_ax_2: "\<forall>X Y. X \<subseteq> Y \<longrightarrow> rk X \<le> rk Y" and
-matroid_ax_3: "\<forall>X Y. rk (X \<union> Y) + rk (X \<inter> Y) \<le> rk X + rk Y"
+matroid_ax_1a: "rk X \<ge> 0" (* Useless if rk is defined with values in \<nat>, not \<int> *) and
+matroid_ax_1b: "rk X \<le> card X" and
+matroid_ax_2: "X \<subseteq> Y \<longrightarrow> rk X \<le> rk Y" and
+matroid_ax_3: "rk (X \<union> Y) + rk (X \<inter> Y) \<le> rk X + rk Y"
 
 (* To capture higher projective geometry, we need to introduce the following additional axioms *)
 axiomatization where
-rk_ax_singleton: "\<forall>P. rk {P} \<ge> 1" and
-rk_ax_couple: "\<forall>P Q. P \<noteq> Q \<longrightarrow> rk {P,Q} \<ge> 2" and
-rk_ax_pasch: "\<forall>A B C D. rk {A,B,C,D} \<le> 3 \<longrightarrow> (\<exists>J. rk {A,B,J} = 2 \<and> rk {C,D,J} = 2)" and
-rk_ax_3_pts: "\<forall> A B. \<exists>C. rk {A,B,C} = 2 \<and> rk {B,C} = 2 \<and> rk {A,C} = 2" and
+rk_ax_singleton: "rk {P} \<ge> 1" and
+rk_ax_couple: "P \<noteq> Q \<longrightarrow> rk {P,Q} \<ge> 2" and
+rk_ax_pasch: "rk {A,B,C,D} \<le> 3 \<longrightarrow> (\<exists>J. rk {A,B,J} = 2 \<and> rk {C,D,J} = 2)" and
+rk_ax_3_pts: "\<exists>C. rk {A,B,C} = 2 \<and> rk {B,C} = 2 \<and> rk {A,C} = 2" and
 rk_ax_dim: "\<exists>A B C D. rk {A,B,C,D} \<ge> 4"
 
 (* Note that the rank-based axioms system above deals only with points. 
