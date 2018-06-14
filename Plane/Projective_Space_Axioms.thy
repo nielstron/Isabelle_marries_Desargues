d30c43bea9a40e81dc2261998b41e722b9e4c3cf
Anthony Bordg <bordg.anthony@gmail.com>
2018-06-14 15:55:47 +0100

    Update Projective_Space_Axioms.thy

--- Plane/Projective_Space_Axioms.thy
+++ Plane/Projective_Space_Axioms.thy
@@ -2,16 +2,16 @@ theory Projective_Space_Axioms
   imports Main
 begin
 
-(*
-Author: Anthony Bordg, University of Cambridge, apdb3@cam.ac.uk .
-  
-Contents:
-- We introduce the types of points and lines and an incidence relation between them.
-- A set of axioms for the (3-dimensional) projective space. 
-An alternative set of axioms could use planes as basic objects in addition to points and lines  
-*)
+(* Author: Anthony Bordg, University of Cambridge, apdb3@cam.ac.uk .*)
 
-(*** The axioms of the Projective Space ***)
+text \<open>
+Contents:
+\<^item> We introduce the types of points and lines and an incidence relation between them.
+\<^item> A set of axioms for the (3-dimensional) projective space. 
+An alternative set of axioms could use planes as basic objects in addition to points and lines  
+\<close>
+
+section \<open>The axioms of the Projective Space\<close>
 
 (* One has a type of points *)
 typedecl "points"
@@ -76,13 +76,6 @@ definition distinct3_line :: "lines \<Rightarrow> lines \<Rightarrow> lines \<Ri
 axiomatization where
 ax5: "distinct3_line l1 l2 l3 \<longrightarrow> (\<exists>l4 J1 J2 J3. distinct3 J1 J2 J3 \<and> 
 meet_in l1 l4 J1 \<and> meet_in l2 l4 J2 \<and> meet_in l3 l4 J3)"
-
-(* 
-Biblio.:
-- Nicolas Magaud, Julien Narboux, Pascal Schreck; coq-projective-geometry; code available at
-https://github.com/coq-contribs/projective-geometry
-- Coxeter; Projective Geometry; Springer, 1987. 
-*)
 
 
 end
